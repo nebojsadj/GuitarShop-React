088ab28e28da382a33a63f15f0273e8e7289b1da
nebojsadj <djordjevicn87@gmail.com>
2020-10-12 12:56:19 +0200

    moved Router

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import Navbar from "./components/Navbar";
-import { BrowserRouter, Route } from "react-router-dom";
+import { Route } from "react-router-dom";
 import Guitars from "./components/Guitars";
 import ElectricGuitars from "./components/Electric/ElectricGuitars";
 import AcousticGuitars from "./components/Acoustic/AcousticGuitars";
@@ -38,35 +38,33 @@ function App() {
   };
 
   return (
-    <div>
-      <BrowserRouter>
-        <Navbar />
-        <Modal guitarToCart={guitarToCart} />
-        <Route path="/" exact>
-          <Guitars />
-        </Route>
-        <Route path="/electric">
-          <ElectricGuitars
-            displayMore={displayMore}
-            addToCart={addToCart}
-            inCart={inCart}
-          />
-        </Route>
-        <Route path="/acoustic">
-          <AcousticGuitars
-            displayMore={displayMore}
-            addToCart={addToCart}
-            inCart={inCart}
-          />
-        </Route>
-        <Route path="/display/:id">
-          <Display guitar={guitar} addToCart={addToCart} inCart={inCart} />
-        </Route>
-        <Route path="/cart">
-          <MyCart guitars={guitarToCart} removeFromCart={removeFromCart} />
-        </Route>
-      </BrowserRouter>
-    </div>
+    <>
+      <Navbar />
+      <Modal guitarToCart={guitarToCart} />
+      <Route path="/" exact>
+        <Guitars />
+      </Route>
+      <Route path="/electric">
+        <ElectricGuitars
+          displayMore={displayMore}
+          addToCart={addToCart}
+          inCart={inCart}
+        />
+      </Route>
+      <Route path="/acoustic">
+        <AcousticGuitars
+          displayMore={displayMore}
+          addToCart={addToCart}
+          inCart={inCart}
+        />
+      </Route>
+      <Route path="/display/:id">
+        <Display guitar={guitar} addToCart={addToCart} inCart={inCart} />
+      </Route>
+      <Route path="/cart">
+        <MyCart guitars={guitarToCart} removeFromCart={removeFromCart} />
+      </Route>
+    </>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -3,5 +3,11 @@ import ReactDOM from "react-dom";
 import "bootstrap/dist/css/bootstrap.css";
 import App from "./App";
 import "./App.css";
+import { BrowserRouter } from "react-router-dom";
 
-ReactDOM.render(<App />, document.querySelector("#root"));
+ReactDOM.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.querySelector("#root")
+);
