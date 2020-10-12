cb82aae6d7fe323d7cb14899fe8779e3f45caf62
nebojsadj <djordjevicn87@gmail.com>
2020-10-12 14:22:02 +0200

    removed allAcoustic

--- src/components/Acoustic/AcousticGuitars.js
+++ src/components/Acoustic/AcousticGuitars.js
@@ -3,21 +3,22 @@ import { acousticData } from "../Data";
 import Acoustic from "./Acoustic";
 
 function AcousticGuitars({ displayMore, addToCart, inCart }) {
-  let allAcoustic = acousticData.map((acoustic, index) => (
-    <div className="col-12" key={index}>
-      <Acoustic
-        acoustic={acoustic}
-        displayMore={displayMore}
-        addToCart={addToCart}
-        inCart={inCart}
-      />
-    </div>
-  ));
   return (
     <div className="container">
       <div className="row">
         <div className="col-10 offset-1">
-          <div className="row">{allAcoustic}</div>
+          <div className="row">
+            {acousticData.map((acoustic, index) => (
+              <div className="col-12" key={index}>
+                <Acoustic
+                  acoustic={acoustic}
+                  displayMore={displayMore}
+                  addToCart={addToCart}
+                  inCart={inCart}
+                />
+              </div>
+            ))}
+          </div>
         </div>
       </div>
     </div>
