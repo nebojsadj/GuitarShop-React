dfb7a784a00aedff8860ee82f7bac7d155960c15
Nebojsa <djordjevicn87@gmail.com>
2020-11-26 18:17:57 +0100

    added navbar icons

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,21 +1,40 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import { Link, NavLink } from "react-router-dom";
+import Badge from "@material-ui/core/Badge";
+import ShoppingCartIcon from "@material-ui/icons/ShoppingCart";
+import HomeIcon from "@material-ui/icons/Home";
 
 function Navbar() {
+  const inCart = useSelector((state) => state.guitars.inCart);
+
   return (
     <nav className="navbar navbar-nav navbar-expand bg-dark">
-      <Link to="/" className="navbar-brand text-danger ml-4">
+      <Link to="/" className="navbar-brand text-light ml-4">
         Guitars shop
       </Link>
       <ul className="navbar-nav ml-auto">
         <li className="nav-item">
-          <NavLink to="/" exact className="btn btn-outline-danger text-light">
-            Guitars
+          <NavLink to="/" exact className="nav">
+            <HomeIcon style={{ width: "45px", height: "45px" }} />
           </NavLink>
         </li>
         <li className="nav-item">
-          <NavLink to="/cart" className="btn btn-outline-danger text-light">
-            Your Cart
+          <NavLink to="/cart" className="nav ml-2">
+            <Badge
+              badgeContent={inCart.length}
+              color="secondary"
+              style={{ marginTop: "5px" }}
+            >
+              <ShoppingCartIcon
+                style={{
+                  color: "white",
+                  width: "40px",
+                  height: "40px",
+                  marginLeft: "10px",
+                }}
+              />
+            </Badge>
           </NavLink>
         </li>
       </ul>
